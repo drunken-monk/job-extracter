e9f19d5c8a8f9a7277e5ba2f51376c3208b2c474
drunkenmonk <aoitori_@naver.com>
2020-09-05 11:50:39 +0000

    remoteok scrapper complete

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from indeed import get_jobs_indeed
 from so import get_jobs_so
 from wwr import get_jobs_wwr
+from remoteok import get_jobs_remoteok
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,13 +12,13 @@ os.system("clear")
 dict_idx_sites = {
   0: ["stack overflow", get_jobs_so],
   1: ["wwr", get_jobs_wwr],
-  2: ["stack overflow", get_jobs_so],
+  2: ["remoteok", get_jobs_remoteok],
   3: ["indeed", get_jobs_indeed],
 }
 
 DESIRE_KEYWORD = "python"
 DESIRE_PAGES = None
-SELECTED_SITES = [1]
+SELECTED_SITES = [2]
 
 jobs = []
 
@@ -27,34 +29,40 @@ for site_idx in SELECTED_SITES:
 for job in jobs:
   print(job,"\n")
 
+
 '''
 keyword = "python"
-url = f"https://weworkremotely.com/remote-jobs/search?term={keyword}&utf8=%E2%9C%93"
+url = f"https://remoteok.io/remote-{keyword}-jobs"
 
 result = requests.get(url)
+
 soup = BeautifulSoup(result.text, "html.parser")
-contents = soup.find("div", {"id": "job_list"})
-items = contents.find_all("li")
+contents = soup.find("table", {"id": "jobsboard"})
+items = contents.find_all("tr", {"class": "job"})
 
 for item in items:
-  SITE_NAME = "weworkremotely"
-  title = item.find("span", {"class": "title"})
-  if title == None:
+
+  SITE_NAME = "remoteok"
+  id = item["data-id"]
+  item = item.find("td", {"class": "company position company_and_position_mobile"})
+  if id == None:
     continue
-  title = item.find("span", {"class": "title"}).get_text(strip=True)
-  company = item.find("span", {"class": "company"}).get_text(strip=True)
+  #print(id)
+  title = item.find("h2").get_text(strip=True)
+  company = item.find("h3").get_text(strip=True)
   try:
-    company_anchor = item.select_one("li > a")["href"]
-    company_anchor = f"https://weworkremotely.com{company_anchor}"
+    company_anchor = f"https://remoteok.io/l/{id}"
   except:
     company_anchor = "No Link"
   try:
-    location = item.find("span", {"class": "region company"}).get_text(strip=True)
+    location = item.find("span", {"class": "location"}).get_text(strip=True)
+    print(location)
   except:
     location = "Anywhere"
+    print(location)
   
   salary = "not opened"
   
-  print(company_anchor)
-
+  #print(company_anchor)
+  
 '''
